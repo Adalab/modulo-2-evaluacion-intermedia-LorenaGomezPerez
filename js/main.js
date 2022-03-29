56d8ead183a873f0f734607eb23717fbdc2c736c
LorenaGomezPerez <lorenagomezperez@gmail.com>
2022-03-29 11:25:30 +0200

    styles css

--- js/main.js
+++ js/main.js
@@ -6,15 +6,22 @@ const playButton = document.querySelector(".js-button");
 const selectNumber = document.querySelector(".js-number");
 const textPlay = document.querySelector(".js-play");
 const randomNumber = getRandomNumber(6);
+const balance = document.querySelector(".js-money-balance");
 
 
+// Funciones y eventos
 
+
+    // Números aleatorios
     function getRandomNumber(max) { 
     return Math.ceil(Math.random() * max); 
   } 
 
     function chooseNumber(){
       const number = parseInt(selectNumber.value);
+
+
+      // Condicional
 
 
    if (number === randomNumber){
@@ -24,12 +31,29 @@ const randomNumber = getRandomNumber(6);
     }
    }
 
+   // Función cuando hago "click"
+
     function handleButton (event){
        event.preventDefault();
        chooseNumber();
    }
 
     playButton.addEventListener("click", handleButton);
+
+    // Cambiar saldo (soy incapaz de hacer el Bonus)
+
+    function balanceChange (balance){
+        return balance;
+
+    if (number === randomNumber){
+            balance.innerHTML = "saldo $(amount*2)";
+        }
+
+
+
+    } 
+
+
  
    
 
