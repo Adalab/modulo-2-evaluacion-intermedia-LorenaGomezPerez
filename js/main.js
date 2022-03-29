30be8a5446f030a9b2a3690a82a50ac2276af6bf
LorenaGomezPerez <lorenagomezperez@gmail.com>
2022-03-29 10:10:41 +0200

    function button

--- js/main.js
+++ js/main.js
@@ -1,2 +1,38 @@
 "use strict";
 
+// Elementos que necesitamos de HTML
+
+const playButton = document.querySelector(".js-button");
+const selectNumber = document.querySelector(".js-number");
+const textPlay = document.querySelector(".js-play");
+const randomNumber = getRandomNumber(6);
+
+
+
+    function getRandomNumber(max) { 
+    return Math.ceil(Math.random() * max); 
+  } 
+
+    function chooseNumber(){
+      const number = parseInt(selectNumber.value);
+
+
+   if (number === randomNumber){
+      textPlay.innerHTML = "Has ganado el doble de lo apostado";
+    } 
+    else {textPlay.innerHTML = "Has perdido lo apostado";
+    }
+   }
+
+    function handleButton (event){
+       event.preventDefault();
+       chooseNumber();
+   }
+
+    playButton.addEventListener("click", handleButton);
+ 
+   
+
+  
+
+
